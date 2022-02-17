32643ee01719aca19222ca1faf47127c69f92fa6
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2022-02-17 18:29:32 +0800

    add firegraphics

--- src/FireGraphics.cpp
+++ src/FireGraphics.cpp
@@ -1,23 +1,25 @@
 #include "FireGraphics.hpp"
+#include "Units.hpp"
 
 namespace fsim
 {
-    FireGraphics::FireGraphics(Node* fireSourceNode, const FloorLabel& floor, sf::Texture* iconTexture)
-        : node(fireSourceNode)
+    FireGraphics::FireGraphics(Node* fireSourceNode, const FloorLabel& floor, sf::Texture* iconTexture,  const float& heatFluxValue_)
+        : node(fireSourceNode), heatFluxValue(heatFluxValue_)
     {
 
-        if (floor == FloorLabel::GROUND)
-        {
+        // if (floor == FloorLabel::GROUND)
+        // {
             sprite.setTexture(*iconTexture);
             sprite.setOrigin(sf::Vector2f(sprite.getTexture()->getSize().x/2.0f, sprite.getTexture()->getSize().y/2.0f));
             sprite.setScale(0.1f, 0.1f);
-            area.setRadius(56.32184847f);
+            area.setRadius(fsim::units::STANDARD_HEAT_FLUX_RADIUS_PIXELS);
             area.setFillColor(sf::Color(255,115, 0, 90.0f));
             area.setOrigin(sf::Vector2f(area.getRadius(), area.getRadius()));
-            const float t_half_size = 3.415f/2.0f;
-            area.setPosition(sf::Vector2f(fireSourceNode->col * 3.415f + t_half_size, fireSourceNode->row * 3.415f + t_half_size));   
-            sprite.setPosition(sf::Vector2f(fireSourceNode->col * 3.415f + t_half_size, fireSourceNode->row * 3.415f + t_half_size));   
-        }
+            const float& unit_s = fsim::units::UNIT_SIZE_IN_PIXELS;
+            const float t_half_size = unit_s/2.0f;
+            area.setPosition(sf::Vector2f(fireSourceNode->col * unit_s + t_half_size, fireSourceNode->row * unit_s + t_half_size));   
+            sprite.setPosition(sf::Vector2f(fireSourceNode->col * unit_s + t_half_size, fireSourceNode->row * unit_s + t_half_size));   
+        // }
     }
 
     FireGraphics::~FireGraphics() {}
--- src/FireGraphics.hpp
+++ src/FireGraphics.hpp
@@ -1,15 +1,16 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "Node.hpp"
+#include "Constants.hpp"
 
-enum FloorLabel { GROUND = 0, SECOND = 1, THIRD = 2, FOURTH = 3 };
+// enum FloorLabel { GROUND = 0, SECOND = 1, THIRD = 2, FOURTH = 3 };
 
 namespace fsim
 {
     class FireGraphics
     {
     public:
-        FireGraphics(Node* fireSourceNode, const FloorLabel& floor, sf::Texture* iconTexture);
+        FireGraphics(Node* fireSourceNode, const FloorLabel& floor, sf::Texture* iconTexture, const float& heatFluxValue_);
         ~FireGraphics();
 
         // Draws the sprite and the danger area
@@ -17,6 +18,7 @@ namespace fsim
 
     public:
         Node* node; // Fire point node
+        float heatFluxValue;
 
     private:
         sf::Sprite      sprite; // Fire icon
